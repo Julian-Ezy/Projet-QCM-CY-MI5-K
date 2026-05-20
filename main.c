0cd99f34c992651b9db1ee62007aed3ee31bc5b0
Julian-Ezy <julian.pinto4@orange.fr>
2026-05-20 16:42:58 +0000

    upd

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 int main() {
     char mdp[20] = "1234";
+    int n;
     FILE *f = fopen("mdp.txt", "r");
     if (f != NULL) {
         if (fgets(mdp, sizeof(mdp), f) == NULL) {
@@ -8,5 +9,13 @@ int main() {
         fclose(f);
     }
     choix_utilisateur(mdp);
+    do{
+        printf("\nVoulez vous quitter le programme ?");
+        printf("\nTapez \"1\" si oui et \"2\" sinon");
+        scanf("%d",&n);
+        if ( n == 2 ){
+            choix_utilisateur(mdp);
+        }
+    }while( n != 1 && n != 2);
     return 0;
 }
